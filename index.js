afb0f000d9a084e2ee2ff5f0f255a406f4be1dc0
esamohammad <esa.ce2020@gmail.com>
2023-05-20 13:53:23 +0600

    API MAKE -POST , BOOKINGS

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const jwt = require('jsonwebtoken');
+
 require('dotenv').config()
-
-
 
 const app = express()
 const port = process.env.PORT || 5000;
-
-
-
-
 
 //!middlewear
 app.use(cors())
@@ -21,8 +15,8 @@ app.use(express.json())
 
 //!mongodb credentials
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ddkgs5g.mongodb.net/?retryWrites=true&w=majority`;
-
 console.log(uri)
+
 
 
 const client = new MongoClient(uri, {
@@ -34,85 +28,131 @@ const client = new MongoClient(uri, {
 });
 
 
+//!main
 async function run() {
   try {
 
     //!collection--DB
     const categoriesCollection = client.db('wood_sell').collection('categories');
+    const productsCollection = client.db('wood_sell').collection('products');
+    const itemsCollection = client.db('wood_sell').collection('items');
+    const advertiseCollection = client.db('wood_sell').collection('advertise');
+    const bookingsCollection = client.db('wood_sell').collection('bookings');
 
-
-
-
-
-
-
-
-//!Categories api all categories
+    //!***
+    //!=========================================
+    //!All Categories
     app.get('/categories', async (req, res) => {
       const query = {};
-      const result = await categoriesCollection.find(query).toArray()
-      res.send(result)
+      const cursor = categoriesCollection.find(query);
+      const categories = await cursor.toArray();
+      res.send(categories);
     })
 
 
-
-
-//! Category api single category
+    //!=========================================
+    //!Single category
     app.get('/categories/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: ObjectId(id) }
+      const filter = { _id: new ObjectId(id) }
       const result = await categoriesCollection.findOne(filter)
       res.send(result)
     })
+
+    //!=========================================
+    //!All products
+    app.get('/products', async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    })
+
+
+    //!=========================================
+    //!Single product by id
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const product = await productsCollection.findOne(query);
+      res.send(product)
+    })
+
+
+    //!***
+    //!=========================================
+    //! product filter by category. 01,02,03,04 .
+    app.get('/items/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { category_id: id }
+      const category = await itemsCollection.find(query).toArray();
+      res.send(category);
+    })
+
+
+    //!=========================================
+    //!All advertise
+    app.get('/advertise', async (req, res) => {
+      const query = {};
+      const cursor = advertiseCollection.find(query);
+      const advertise = await cursor.toArray();
+      res.send(advertise);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //!=========================================
+    //!Single advertise by id
+    app.get('/advertise/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const advertise = await advertiseCollection.findOne(query);
+      res.send(advertise)
+    })
+
+
+
+    //!=========================================
+    //!Post Api -Bookings
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body
+      console.log(booking);
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+
+
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
   finally {
-    
-    
-
 
   }
 }
 run().catch(console.dir);
 
 
-
-//! json file import
-const categories = require("./categories.json");
-
-
 app.get("/", (req, res) => {
   res.send("Wood Sell is running on Server")
 });
-
-
-
-
-//! data api get....(all Data api)
-app.get('/categories', (req, res) => {
-  res.send(categories)
-})
-
-
-//! data api get....(Single id --Data api)
-app.get("/categories/:id", (req, res) => {
-  const id = req.params.id;
-  const single_category = categories.find(category => category._id === id) ||{};
-  res.send(single_category)
-})
-
 
 
 //! cmd showed window--
